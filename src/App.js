432c9e763fc3ccc2b991cd783b8ea4cbcafbf453
phamnguyen <nguyenptse160896@fpt.edu.vn>
2022-06-08 20:52:40 +0700

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Hello from "./hello.js";
+
+function formatName(user) {
+  return user.firstName + " " + user.lastName;
+}
+
+const user = {
+  firstName: "Happer",
+  lastName: "Perez",
+};
 
 function App() {
+  const name = "";
+  const element = "Hello," + formatName(user);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{Hello} </h1>
     </div>
   );
 }
---
+++ src/hello.js
@@ -0,0 +1,11 @@
+import React from "react";
+class Hello extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>Em yeu co</h1>;
+  }
+}
+
+export default Hello;
